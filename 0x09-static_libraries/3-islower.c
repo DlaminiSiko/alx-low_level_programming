7a735497feddf29d7849e00219c15ffb88ffd910
Dlamini.Siko <dlamini.sphe01@gmail.com>
2023-11-01 05:00:07 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character being checked
+ *
+ * Return: 1 if its lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
