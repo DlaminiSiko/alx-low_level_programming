9dbae3e2d4557fa06dc99c9cc21a0e0344106e5d
Dlamini.Siko <dlamini.sphe01@gmail.com>
2023-11-03 04:37:04 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * malloc_checked - allocates memory
+ * malloc_checked - Allocates memory
  * @b: the size to allocate
  *
  * Return: Nothing.
@@ -16,5 +16,6 @@ void *malloc_checked(unsigned int b)
 
 	if (p == NULL)
 		exit(98);
+
 	return (p);
 }
